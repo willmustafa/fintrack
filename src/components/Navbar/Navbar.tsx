7ac1a94aa39ca7d9127ab791b89c431afc831e5a
willian mustafa <willian@integra.do>
2024-05-06 17:30:07 -0300

    Update navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+'use client'
+
+import React, {useEffect, useState} from 'react';
 import Image from "next/image";
+import styles from './Navbar.module.css'
 
 function Navbar() {
+    const [scrolling, setScrolling] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScrolling(true);
+            } else {
+                setScrolling(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className="navbar navbar-expand-lg fixed-top">
+        <header className={scrolling ? `${styles.scrolled} navbar navbar-expand-lg fixed-top` : 'navbar navbar-expand-lg fixed-top'}>
             <div className="container">
                 <div className="navbar-header">
                     <Image

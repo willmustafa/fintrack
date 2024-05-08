b56a23599a6b19ddb5405ee4ae97e4ff693a1886
Willian Mustafa <willian@integra.do>
2024-05-08 07:49:47 -0300

    dashboard init

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import NavbarDashboard from '@/components/NavbarDashboard/NavbarDashboard';
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div>
+            <NavbarDashboard />
+        </div>
+    );
+}
+
+export default Dashboard;

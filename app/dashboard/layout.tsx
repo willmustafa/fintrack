67768246ded338961be84b536b908ab8be0d899b
Willian <willian@integra.do>
2025-02-26 13:05:44 -0300

    dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,21 @@
+import Sidebar from "@/components/sidebar";
+
 export default function PricingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        {children}
-      </div>
-    </section>
+    <>
+      <Sidebar />
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <Toaster position="top-right" />
+          {children}
+        </div>
+      </section>
+    </>
   );
 }
+
+import { Toaster } from "react-hot-toast";
---
+++ components/sidebar.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import {
+  ForwardRefExoticComponent,
+  RefAttributes,
+  SVGProps,
+  useState,
+} from "react";
+import Image from "next/image";
+import { Home } from "iconoir-react";
+import LogoSmall from "@/public/logo-small.png";
+import Logo from "@/assets/logo.svg";
+import NextLink from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+interface SidebarLinkProps {
+  path: string;
+  name: string;
+  icon?: ForwardRefExoticComponent<
+    Omit<SVGProps<SVGSVGElement>, "ref"> & RefAttributes<SVGSVGElement>
+  >;
+  collapsed: boolean;
+}
+
+function SidebarLink({ path, name, collapsed }: SidebarLinkProps) {
+  const pathname = usePathname();
+
+  return (
+    <NextLink
+      key={name}
+      className={clsx(
+        "flex items-center space-x-3 py-3 px-6 hover:bg-gray-700 rounded-md cursor-pointer",
+        pathname === path ? "active" : "",
+      )}
+      role="menuitem"
+      tabIndex={0}
+      aria-label={name}
+      href={path}
+    >
+      {/*<icon className="h-6 w-6" aria-hidden="true" />*/}
+      {!collapsed && <span>{name}</span>}
+    </NextLink>
+  );
+}
+
+export default function Sidebar() {
+  const [collapsed, setCollapsed] = useState(false);
+  const user = {
+    name: "Thaila N",
+    avatar:
+      "https://www.tubefilter.com/wp-content/uploads/2018/09/anna-akana-1400x825.jpg",
+  };
+
+  const menuItems = [
+    { name: "Dashboard", link: "/dashboard", icon: Home },
+    { name: "Extrato", link: "/extrato", icon: Home },
+    { name: "Cartões", link: "/cartoes", icon: Home },
+    { name: "Bancos", link: "/bancos", icon: Home },
+  ];
+
+  return (
+    <aside
+      className={`h-screen fixed left-0 text-white p-4 flex flex-col gap-5 transition-all duration-300 ${collapsed ? "w-[102px]" : "w-64"}`}
+      role="navigation"
+      aria-label="Sidebar Navigation"
+      onClick={() => setCollapsed(!collapsed)}
+    >
+      <div className="flex items-center justify-center gap-2 bg-[var(--dark-blue)] rounded-[50px] h-[78px]">
+        {!collapsed ? (
+          <>
+            <Image src={LogoSmall} alt="" width={24} height={33} />
+            <Image src={Logo} alt="" width={100} height={16} />
+          </>
+        ) : (
+          <Image src={LogoSmall} alt="" width={24} height={33} />
+        )}
+      </div>
+
+      <div className="bg-[var(--dark-blue)] rounded-3xl h-full flex flex-col py-10">
+        <nav className="flex-1">
+          {menuItems.map(({ name, icon: Icon, link }) => (
+            <SidebarLink
+              name={name}
+              path={link}
+              key={name}
+              collapsed={collapsed}
+            />
+          ))}
+        </nav>
+
+        <div className="mt-auto">
+          <div
+            className="flex items-center space-x-3 p-3 hover:bg-gray-700 rounded-md cursor-pointer px-6"
+            role="button"
+            tabIndex={0}
+            aria-label="User Profile"
+          >
+            <div
+              className="w-8 h-8 rounded-full bg-no-repeat bg-cover bg-center"
+              style={{ backgroundImage: `url(${user.avatar})` }}
+            ></div>
+            {!collapsed && <span>Thaila N</span>}
+          </div>
+          <div
+            className="flex items-center space-x-3 p-3 hover:bg-gray-700 rounded-md cursor-pointer px-6"
+            role="button"
+            tabIndex={0}
+            aria-label="Sign Out"
+          >
+            <Home className="h-6 w-6" aria-hidden="true" />
+            {!collapsed && <span>Sair</span>}
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+}

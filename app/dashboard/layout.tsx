bd977236e079dd21bf2b88f4884204b24be1cde9
willian <willian@integra.do>
2025-02-26 18:54:46 -0300

    date

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,8 @@ import Sidebar from "@/components/sidebar";
 import { Toaster } from "react-hot-toast";
 import { useState } from "react";
 import clsx from "clsx";
+import DatePicker from "react-datepicker";
+import "@/assets/styles/datepicker.css";
 
 export default function PricingLayout({
   children,
@@ -11,6 +13,7 @@ export default function PricingLayout({
   children: React.ReactNode;
 }) {
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
     <main className="bg-slate-50">
@@ -20,10 +23,24 @@ export default function PricingLayout({
       />
       <section
         className={clsx(
-          "flex flex-col items-center justify-center gap-4 py-8 md:py-10 transition-all duration-300",
+          "flex flex-col items-center justify-center gap-4 pb-8 md:pb-10 transition-all duration-300",
           !isSidebarCollapsed ? "ml-64" : "ml-28",
         )}
       >
+        <nav className="flex flex-row py-6 container justify-between items-center h-24">
+          <h2 className="text-base font-semibold uppercase">Dashboard</h2>
+          <div className="relative">
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) =>
+                setSelectedDate(new Date(date?.toString() ?? ""))
+              }
+              dateFormat="MM/yyyy"
+              showMonthYearPicker
+            />
+          </div>
+          <div>Ab</div>
+        </nav>
         <div className="inline-block text-center justify-center min-h-screen container m-auto">
           <Toaster position="top-right" />
           {children}

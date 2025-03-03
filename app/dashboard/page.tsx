b28b526974b66a1a42343e4cf36364a71f71a780
willian <willian@integra.do>
2025-03-03 17:24:51 -0300

    Tips card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import CompactCard from "@/components/CompactCard";
 import { ArrowsUpFromLine } from "lucide-react";
 import CostsDivision from "@/components/costs-division";
+import TipsCard from "@/components/tips-card";
+import Neutral1 from "@/assets/tips/neutral-1.png";
 
 export default function DashboardPage() {
   return (
-    <div className="w-full">
+    <div className="w-full grid grid-cols-1 gap-6">
       <div className="grid grid-cols-4 gap-6">
         <CompactCard title="Saldo do mês" value={500}>
           <ArrowsUpFromLine width={16} />
@@ -19,8 +21,13 @@ export default function DashboardPage() {
           <ArrowsUpFromLine width={16} />
         </CompactCard>
       </div>
-      <div className="grid grid-cols-2">
-        <CostsDivision />
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-y-6">
+        {/*<CostsDivision />*/}
+        <TipsCard
+          title="Semana tranquila"
+          message="Parece que essa semana não houveram nem gastos nem ganhos, as vezes é bom relaxar, se permita descansar."
+          imageUrl={Neutral1}
+        />
       </div>
     </div>
   );
---
+++ components/tips-card.tsx
@@ -0,0 +1,31 @@
+import { Card, CardBody } from "@heroui/card";
+import Image, { StaticImageData } from "next/image";
+
+export interface TipsCardProps {
+  title: string;
+  imageUrl: StaticImageData;
+  message: string;
+}
+
+export default function TipsCard({ title, imageUrl, message }: TipsCardProps) {
+  // @ts-ignore
+  return (
+    <Card className="bg-[#9086E9] text-white overflow-visible">
+      <CardBody className="flex flex-row overflow-visible min-h-32 content-center">
+        <div className="w-1/4 relative">
+          <Image
+            className="absolute -bottom-[.75rem] left-4 w-5/6"
+            alt=""
+            src={imageUrl}
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="w-3/4 self-center ps-5">
+          <h3 className="text-xl font-semibold">{title}</h3>
+          <p className="text-sm mt-3">{message}</p>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}

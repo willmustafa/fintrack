cda205d42d1a0964c5b7963187b1f62dfdedbfa7
Willian Mustafa <willianfmustafa@gmail.com>
2026-08-08 14:59:48 +0000

    reset

--- helpers/numbers.ts
+++
@@ -1,9 +0,0 @@
-import { NumberFormatOptions } from "@internationalized/number";
-
-export function toCurrency(value: number, opts?: NumberFormatOptions): string {
-  return new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    ...opts,
-  }).format(value);
-}
